48be7cf9b711a61b1c5b8580de232b8483407637
Kemal <demirelkd0696@gmail.com>
2020-10-28 14:09:01 +0300

    add movie app

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-style.js
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    geners: [],
+    privat: false
+};
+
+const a = prompt("Один из проследних просмотренных фильмов?", ''),
+      b = prompt("На сколько его оцениваете?", ''),
+      c = prompt("Один из проследних просмотренных фильмов?", ''),
+      d = prompt("На сколько его оцениваете?", '');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB.movies);
+
